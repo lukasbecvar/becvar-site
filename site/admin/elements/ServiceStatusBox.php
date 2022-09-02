f4a902aa9121ef4702b79028897b5193766c9e46
lordbecvold <lordbecvold@gmail.com>
2022-09-02 11:17:43 +0200

    sort fix

--- site/admin/elements/ServiceStatusBox.php
+++ site/admin/elements/ServiceStatusBox.php
@@ -4,16 +4,6 @@
         <?php //The main page of admin with server status
             //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            //Get SSH status
-            if ($servicesController->isServiceInstalled("sshd")) {
-
-                if ($servicesController->ifServiceActive("sshd")) {
-                    echo '<p class="card-text">SSHD: <span class="text-success">enabled</span> <strong>[<a href="?admin=executeTask&command=sshdStop">STOP</a>]<span></span></strong></p>';
-                } else {
-                    echo '<p class="card-text">SSHD: <span class="text-warning">disabled</span> <strong><span>[<a href="?admin=executeTask&command=sshdStart">START</a>]</span></strong></p>';
-                }
-            }
-
             //Get UFW status
             if ($servicesController->isServiceInstalled("ufw")) {
 
@@ -23,6 +13,24 @@
                     echo '<p class="card-text">UFW[Firewall]: <span class="text-warning">disabled</span> <strong><span>[<a href="?admin=executeTask&command=ufwStart">START</a>]</span></strong></p>';
                 }
             }
+
+            //Get team speak status
+            if ($servicesController->isServiceInstalled("ts3server")) {
+                if ($servicesController->ifProcessRunning("ts3server")) {
+                    echo '<p class="card-text">TeamSpeak: <span class="text-success">online</span> <strong><span>[<a href="?admin=executeTask&command=ts3serverStop">STOP</a>]</span></strong></p>';
+                } else {
+                    echo '<p class="card-text">TeamSpeak: <span class="text-warning">offline</span> <strong><span>[<a href="?admin=executeTask&command=ts3serverStart">START</a>]</span></strong></p>';
+                }
+            } 
+
+            //Get minecraft status
+            if ($servicesController->isServiceInstalled("minecraft")) {
+                if ($responseUtils->serviceOnlineCheck("127.0.0.1", "25565") == "Online") {
+                    echo '<p class="card-text">Minecraft: <span class="text-success">online</span> <strong><span>[<a href="?admin=executeTask&command=minecraftStop">STOP</a>]</span></strong></p>';
+                } else {
+                    echo '<p class="card-text">Minecraft: <span class="text-warning">offline</span> <strong><span>[<a href="?admin=executeTask&command=minecraftStart">START</a>]</span></strong></p>';
+                }
+            } 
 
             //Get openvpn status
             if ($servicesController->isServiceInstalled("openvpn")) {
@@ -51,24 +59,6 @@
                 }
             }
 
-            //Get tor status
-            if ($servicesController->isServiceInstalled("tor")) {
-                if ($servicesController->ifServiceActive("tor")) {
-                    echo '<p class="card-text">Tor: <span class="text-success">online</span> <strong><span>[<a href="?admin=executeTask&command=torStop">STOP</a>]</span></strong></p>';
-                } else {
-                    echo '<p class="card-text">Tor: <span class="text-warning">offline</span> <strong><span>[<a href="?admin=executeTask&command=torStart">START</a>]</span></strong></p>';
-                }
-            }
-
-            //Get minecraft status
-            if ($servicesController->isServiceInstalled("minecraft")) {
-                if ($responseUtils->serviceOnlineCheck("127.0.0.1", "25565") == "Online") {
-                    echo '<p class="card-text">Minecraft: <span class="text-success">online</span> <strong><span>[<a href="?admin=executeTask&command=minecraftStop">STOP</a>]</span></strong></p>';
-                } else {
-                    echo '<p class="card-text">Minecraft: <span class="text-warning">offline</span> <strong><span>[<a href="?admin=executeTask&command=minecraftStart">START</a>]</span></strong></p>';
-                }
-            } 
-
             //Get dubinek status
             if ($servicesController->isServiceInstalled("dubinek")) {
                 if ($servicesController->checkScreenSession("dubinek") == true) {
@@ -78,14 +68,24 @@
                 }
             } 
 
-            //Get team speak status
-            if ($servicesController->isServiceInstalled("ts3server")) {
-                if ($servicesController->ifProcessRunning("ts3server")) {
-                    echo '<p class="card-text">TeamSpeak: <span class="text-success">online</span> <strong><span>[<a href="?admin=executeTask&command=ts3serverStop">STOP</a>]</span></strong></p>';
+            //Get SSH status
+            if ($servicesController->isServiceInstalled("sshd")) {
+
+                if ($servicesController->ifServiceActive("sshd")) {
+                    echo '<p class="card-text">SSHD: <span class="text-success">enabled</span> <strong>[<a href="?admin=executeTask&command=sshdStop">STOP</a>]<span></span></strong></p>';
                 } else {
-                    echo '<p class="card-text">TeamSpeak: <span class="text-warning">offline</span> <strong><span>[<a href="?admin=executeTask&command=ts3serverStart">START</a>]</span></strong></p>';
+                    echo '<p class="card-text">SSHD: <span class="text-warning">disabled</span> <strong><span>[<a href="?admin=executeTask&command=sshdStart">START</a>]</span></strong></p>';
                 }
-            } 
+            }
+
+            //Get tor status
+            if ($servicesController->isServiceInstalled("tor")) {
+                if ($servicesController->ifServiceActive("tor")) {
+                    echo '<p class="card-text">Tor: <span class="text-success">online</span> <strong><span>[<a href="?admin=executeTask&command=torStop">STOP</a>]</span></strong></p>';
+                } else {
+                    echo '<p class="card-text">Tor: <span class="text-warning">offline</span> <strong><span>[<a href="?admin=executeTask&command=torStart">START</a>]</span></strong></p>';
+                }
+            }
             //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         ?>
     </div>
