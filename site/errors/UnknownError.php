1a6d9eaf23f7e91421c2d67b48853c699b6d6cbc
lukas <lordbecvold@gmail.com>
2023-01-31 15:33:12 +0100

    [+] global error pages css

--- site/errors/UnknownError.php
+++ site/errors/UnknownError.php
@@ -8,38 +8,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="icon" href="assets/img/favicon.png" type="image/x-icon"/>
+	<link href="assets/css/errorPage.css" rel="stylesheet">
 	<title>520 Unknown error</title>
 </head>
-<style>
-	* { 
-		padding: 0px;
-		margin: 0px;
-		box-sizing: border-box;
-	}
-
-	body {
-		background: rgb(12, 12, 12);
-	}
-	
-	.mainPage {
-		position: fixed;
-		width: 100%;
-		height: 100%;
-	}
-
-	.errorPageMSG {
-		color: white;
-		font-size: 20px;
-		position: absolute;
-		top: 45%;
-		left: 50%;
-		width: 100%;
-		transform: translate(-50%, -50%);
-		font-family: 'Maven Pro', sans-serif;
-		user-select: none;
-		opacity: 0.8;
-	}
-</style>
 <body class="mainPage">
 	<main>
 		<center><h3 class="errorPageMSG"><strong>Unknown error, please contact the service administrator.<br></strong></h3></center>
