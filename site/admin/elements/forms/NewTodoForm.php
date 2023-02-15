16552c17a59f04e24a74142d686d64ed16ca4998
lukas <lordbecvold@gmail.com>
2023-02-15 16:14:50 +0100

    idk

--- site/admin/elements/forms/NewTodoForm.php
+++ site/admin/elements/forms/NewTodoForm.php
@@ -1,4 +1,4 @@
 <form class="newTodoForm" action="?admin=todos" method="post">
-    <textarea class="todoArea" maxlength="120" name="todoText" class="feedback-input" placeholder="Todo"></textarea>
+    <textarea class="todoArea" maxlength="119" name="todoText" class="feedback-input" placeholder="Todo"></textarea>
     <input class="inputButton todoButton" type="submit" name="submitNewTodo" value="Save">
 </form>
