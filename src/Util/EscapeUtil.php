0a3f0e3bad6aade5004b75860e35878500beaae0
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-03 17:19:44 +0200

    f

--- src/Util/EscapeUtil.php
+++ src/Util/EscapeUtil.php
@@ -9,7 +9,7 @@ namespace App\Util;
 class EscapeUtil
 {
     // replace html tags with secure chars
-    public static function special_chars_strip($string): ?string {
+    public static function special_chars_strip(string $string): ?string {
         return htmlspecialchars($string, ENT_QUOTES);
     }
 }
