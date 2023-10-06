d033b794ca4db6637ac8178f4e5d60da488777d9
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-06 16:58:32 +0200

    [+] project entity

---
+++ src/Entity/Project.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $technology = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $github_link = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTechnology(): ?string
+    {
+        return $this->technology;
+    }
+
+    public function setTechnology(string $technology): static
+    {
+        $this->technology = $technology;
+
+        return $this;
+    }
+
+    public function getGithubLink(): ?string
+    {
+        return $this->github_link;
+    }
+
+    public function setGithubLink(string $github_link): static
+    {
+        $this->github_link = $github_link;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
