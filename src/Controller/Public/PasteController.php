c0b5079942f028c207bb535a79361db71a890303
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-07 12:48:18 +0200

    [+] code paste component

--- src/Controller/Public/PasteController.php
+++ src/Controller/Public/PasteController.php
@@ -2,18 +2,108 @@
 
 namespace App\Controller\Public;
 
+use App\Entity\Paste;
+use App\Helper\LogHelper;
+use App\Util\SecurityUtil;
+use App\Helper\ErrorHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /*
-    Paste controller provides paste/view code paste component
+    Paste controller provides save/view code paste component
 */
 
 class PasteController extends AbstractController
 {
+    private $logHelper;
+    private $errorHelper;
+    private $securityUtil;
+    private $entityManager;
+
+    public function __construct(
+        LogHelper $logHelper, 
+        ErrorHelper $errorHelper, 
+        SecurityUtil $securityUtil,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->logHelper = $logHelper;
+        $this->errorHelper = $errorHelper;
+        $this->securityUtil = $securityUtil;
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/paste', name: 'public_code_paste')]
-    public function viewer()
+    public function new(): Response
     {
-        die('paste');
+        // check if paste submited
+        if (isset($_POST['data'])) {
+        
+            // get data from post & escape
+            $content = $this->securityUtil->escapeString($_POST['data']);
+            $name = $this->securityUtil->escapeString($_POST['file']);
+
+            // get upload date
+            $date = date('d.m.Y H:i:s');
+                
+            // check if maximum lenght reached
+            if (strlen($content) > 60001) {
+
+                // redirect error
+                $this->errorHelper->handleError('error: this paste reached maximum characters 60000', 400);
+
+            } else {
+
+                // save paste to mysql table
+                if (!empty($content)) {
+
+                    // init paste entity
+                    $paste = new Paste();
+
+                    // set paste data
+                    $paste->setName($name);
+                    $paste->setContent($content);
+                    $paste->setDate($date);
+
+                    // insert new paste
+                    try {
+                        $this->entityManager->persist($paste);
+                        $this->entityManager->flush();
+                    } catch (\Exception $e) {
+                        $this->errorHelper->handleError('error to save new paste, error: '.$e->getMessage(), 500);
+                    } 
+                } 
+
+                // log new paste
+                $this->logHelper->log("paste", "saved new paste: ".$name);
+            }
+        }
+
+        return $this->render('public/paste/paste-save.html.twig');
+    }
+
+    #[Route('/paste/view/{token}', name: 'public_code_paste_view')]
+    public function viewer($token)
+    {
+        $content = null;
+     
+        // get paste spec name
+        $name = $this->securityUtil->escapeString($token);
+
+        // get paste data
+        $pasteContent = $this->entityManager->getRepository(Paste::class)->findOneBy(['name' => $name]);
+        
+        // check if paste found
+        if ($pasteContent !== null) {
+            $content = $pasteContent->getContent();
+
+            // replace xss (Escape [XSS Protection])
+            $content = str_replace(array("&lt;", "&gt;"), array("<", ">"), $content);
+        } else {
+            $this->errorHelper->handleError('error paste content is empty!', 404);
+        }
+
+        return $this->render('public/paste/paste-view.html.twig', ['file' => $token, 'paste_content' => $content]);
     }
 }
---
+++ src/Entity/Paste.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PasteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PasteRepository::class)]
+class Paste
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
