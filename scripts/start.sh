75f91b9c4a35e310660b26920c68617a87e85472
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-11 15:14:43 +0200

    vendor check

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,29 @@
+#!/bin/bash
+
+# color codes.
+red_echo () { echo "$(tput setaf 9)$1"; }
+die() { echo "$*" 1>&2 ; exit 1; }
+
+# start server script for development use -> php, database server
+
+# check if vendor installed
+if [ ! -d "vendor/" ]
+then
+    red_echo "Build-error: vendor not found, please install composer"
+    die
+fi
+
+# clear console
+clear
+
+# go to public
+cd public/ 
+
 # start mysql
 sudo systemctl start mysql
 
 # print mysql status
-sudo systemctl status mysql
+systemctl --no-pager status mysql
 
 # start server
 symfony server:start
