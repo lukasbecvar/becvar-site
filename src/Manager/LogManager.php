ffb73fd5f557286f164b1292cad267bfec1c75b6
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-12 10:20:57 +0200

    log reader formated browser name

--- src/Manager/LogManager.php
+++ src/Manager/LogManager.php
@@ -83,22 +83,23 @@ class LogManager
         }
     }
 
-    public function getLogs(string $status, $username, int $page): array
+    public function getLogs(string $status, $username, int $page): ?array
     {
         $repo = $this->entityManager->getRepository(Log::class);
     
-        $perPage = $_ENV['ITEMS_PER_PAGE'];
+        $per_page = $_ENV['ITEMS_PER_PAGE'];
         
         // calculate offsetu
-        $offset = ($page - 1) * $perPage;
+        $offset = ($page - 1) * $per_page;
     
         // get logs from database
         try {
             $queryBuilder = $repo->createQueryBuilder('l')
                 ->where('l.status = :status')
+                ->orderBy('l.id', 'DESC')
                 ->setParameter('status', $status)
                 ->setFirstResult($offset)  
-                ->setMaxResults($perPage);
+                ->setMaxResults($per_page);
     
             $logs = $queryBuilder->getQuery()->getResult();
         } catch (\Exception $e) {
@@ -107,8 +108,15 @@ class LogManager
         }
     
         $this->log('database', 'user: ' . $username . ' viewed logs');
-    
-        return array_reverse($logs);
+
+        // replace browser with formated value for log reader
+        foreach ($logs as $log) {
+            $user_agent = $log->getBrowser();
+            $formated_browser = $this->visitorInfoUtil->getBrowserShortify($user_agent);
+            $log->setBrowser($formated_browser);
+        }
+
+        return $logs;
     }
 
     public function getLogsCount(string $status): int
