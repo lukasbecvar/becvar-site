af81fb3f11158b34f53de1ce1b4f5ba3097a1d70
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-13 11:51:10 +0200

    [+] image browser

---
+++ src/Controller/Admin/MediaBrowserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Manager\AuthManager;
+use App\Manager\DatabaseManager;
+use App\Util\VisitorInfoUtil;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/*
+    Media browser controller provides image-uploader browser
+*/
+
+class MediaBrowserController extends AbstractController
+{
+    private $authManager;
+    private $visitorInfoUtil;
+    private $databaseManager;
+
+    public function __construct(
+        AuthManager $authManager, 
+        VisitorInfoUtil $visitorInfoUtil,
+        DatabaseManager $databaseManager
+    ) {
+        $this->authManager = $authManager;
+        $this->visitorInfoUtil = $visitorInfoUtil;
+        $this->databaseManager = $databaseManager;
+    }
+
+    #[Route('/admin/media/browser/{page}', name: 'admin_media_browser')]
+    public function mediaBrowser(int $page): Response
+    {
+        // check if user logged in
+        if ($this->authManager->isUserLogedin()) {
+     
+            $media = $this->databaseManager->getTableDataByPage('image', $page);
+
+            return $this->render('admin/media-browser.html.twig', [
+                // component properties
+                'is_mobile' => $this->visitorInfoUtil->isMobile(),
+                'is_dashboard' => false,
+
+                // user data
+                'user_name' => $this->authManager->getUsername(),
+                'user_role' => $this->authManager->getUserRole(),
+                'user_pic' => $this->authManager->getUserProfilePic(),
+
+                // media browser data
+                'page' => $page,
+                'media_data' => $media, 
+                'media_count' => count($media),
+                'media_limit' => $_ENV['ITEMS_PER_PAGE']
+            ]);
+        } else {
+            return $this->redirectToRoute('auth_login');
+        }
+    }
+}
