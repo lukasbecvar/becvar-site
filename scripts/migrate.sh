761a426dfec8e3aa1130aefe6d9c5e12f64f2910
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-14 16:41:39 +0200

    [+] apache config

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# create database
+php bin/console doctrine:database:create
+
 # database migration for update database structure
 php bin/console make:migration
 php bin/console doctrine:migrations:migrate
