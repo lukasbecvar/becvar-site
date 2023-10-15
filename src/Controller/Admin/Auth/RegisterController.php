33da326628d547fe174c3ccbc3862bb6ba2e515c
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-15 12:46:36 +0200

    Password match check fix

--- src/Controller/Admin/Auth/RegisterController.php
+++ src/Controller/Admin/Auth/RegisterController.php
@@ -89,50 +89,51 @@ class RegisterController extends AbstractController
                 // check if passwords not match
                 if ($password != $repassword) {
                     $error_msg = 'Your passwords dont match';
-                }
+                } else {
 
-                // get user ip
-                $ip_address = $this->visitorInfoUtil->getIP();
-            
-                // generate token
-                $token = ByteString::fromRandom(32)->toString();
+                    // get user ip
+                    $ip_address = $this->visitorInfoUtil->getIP();
                 
-                // get visitor id
-                $visitor_id = $this->visitorInfoUtil->getVisitorID($ip_address);
+                    // generate token
+                    $token = ByteString::fromRandom(32)->toString();
+                    
+                    // get visitor id
+                    $visitor_id = $this->visitorInfoUtil->getVisitorID($ip_address);
 
-                // password hash
-                $hashed_password = $this->securityUtil->gen_bcrypt($password, 10);
+                    // password hash
+                    $hashed_password = $this->securityUtil->gen_bcrypt($password, 10);
 
-                // default profile pics base64
-                $image_base64 = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBw4RDQ0OEA0QDhANDQ0NDw4NDhsNDg0OFREWFxcTFRUYICggGBolGxMTITEhJSkrLi4uFx8zODMsNygtLisBCgoKDQ0NDg0NDisZFRkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAOYA2wMBIgACEQEDEQH/xAAaAAEAAwEBAQAAAAAAAAAAAAAAAQQFAwIH/8QAMhABAQABAQYEBAQGAwAAAAAAAAECEQMEEiFRkSIxQWEFcYGhQnKxwSMyUoLh8DNi0f/EABUBAQEAAAAAAAAAAAAAAAAAAAAB/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A+qAKgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIebtMf6p3B7HObXH+qd49ygkQkAAAAAAAAAAAAAAAAAAAEWgjLKSa26SKe232/hn1v/jhvG3uV9vSfu5A9Z7TK+eVv1eNEiiNHrHKzytnyqAFnZb5lPPxT7r2y2kyxlmul6shY3Ta2Zaa8ulvJBpCEgAAAAAAAAAAAAAAAAK2/bSTCzXnfT10WMrpLb6c/oyNpncsrlfX7QHkBQAAAAdN2kueOt05uYDZSr7nteLDn5zlVhAAAAAAAAAAAAAAAABX37LTC+9mP+9mau/EbywnvapAAKAAAAAALPw/LxWdcf0aLL3O/wATH31n2aiAAAAAAAAAAAAAAADjvW14cdZ53lAVfiF8WP5f3VXrabS5XW3V5UAAAAAAAAdN3v8AEw/NGqxpdLrPTmv7nvFytmXPSayoLYAAAAAAAAAAAAACp8Qnhntl+y28bXCZY2X1BkD1tMLjdLNHlQAAAAAAAAWdwnjvtjVaRpbnseHHn53z9vZB3SAAAAAAAAAAAAACEgK2/wD/AB/3Ys5o7/PB/dGcAAoAAAAAAtfD74svy/u0FD4dj4sr6Sad19BCQAAAAAAAAAAAAAABz281wyn/AFrJbNjHzx0tnS6AgBQAAAAAkBf+Hzw29clpz3fDhwxl8/V1QAAAAAAAAAAAAAAAAFLf9l5ZSeXnp0XUWAxha2+52S2XWTW6XlZFVQAAAAWNy2VuUvpOf1eNhsLneknnWls8JjJJ5T7+6D0kAAAAAAAAAAAAAAQCRFrxdrjPxTuDoOGW94T8Wvyjllv2Ppjb9gd95vgy+TKd9tvWWUs0klcFAAAAF74deWU95+i4ydhtrjrppz6rOO/T1x7VBdFeb5h1s+ce8dvhfxQHUeZlOsv1egAAAAAAAAAU983jTwzz9b09gdNvvWOPL+a9J6fNT2m9Z3109pycQC29UaJFAAAAAAAAAAAAB0w2+c8sr8rzjmAvbHfZeWU0955f4W5WMsbrvHDdL/Lfsg0hCQAAAAc9vtOHG325fNk2+t875rvxDK+HGS9byU+G9L2BAnhvS9jhvS9lECeG9L2OG9L2BAnhvS9jhvS9gQJ4b0vY4b0vYECeG9L2OG9L2BAnhvS9jhvS9gQJ4b0vY4b0vYECeG9L2OG9L2BAnhvS9jhvS9gQJ4b0vY4b0vYF/cNrrjcb54/otMzdLcc5yvPleXVpoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP/9k=';
+                    // default profile pics base64
+                    $image_base64 = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBw4RDQ0OEA0QDhANDQ0NDw4NDhsNDg0OFREWFxcTFRUYICggGBolGxMTITEhJSkrLi4uFx8zODMsNygtLisBCgoKDQ0NDg0NDisZFRkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAOYA2wMBIgACEQEDEQH/xAAaAAEAAwEBAQAAAAAAAAAAAAAAAQQFAwIH/8QAMhABAQABAQYEBAQGAwAAAAAAAAECEQMEEiFRkSIxQWEFcYGhQnKxwSMyUoLh8DNi0f/EABUBAQEAAAAAAAAAAAAAAAAAAAAB/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A+qAKgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIebtMf6p3B7HObXH+qd49ygkQkAAAAAAAAAAAAAAAAAAAEWgjLKSa26SKe232/hn1v/jhvG3uV9vSfu5A9Z7TK+eVv1eNEiiNHrHKzytnyqAFnZb5lPPxT7r2y2kyxlmul6shY3Ta2Zaa8ulvJBpCEgAAAAAAAAAAAAAAAAK2/bSTCzXnfT10WMrpLb6c/oyNpncsrlfX7QHkBQAAAAdN2kueOt05uYDZSr7nteLDn5zlVhAAAAAAAAAAAAAAAABX37LTC+9mP+9mau/EbywnvapAAKAAAAAALPw/LxWdcf0aLL3O/wATH31n2aiAAAAAAAAAAAAAAADjvW14cdZ53lAVfiF8WP5f3VXrabS5XW3V5UAAAAAAAAdN3v8AEw/NGqxpdLrPTmv7nvFytmXPSayoLYAAAAAAAAAAAAACp8Qnhntl+y28bXCZY2X1BkD1tMLjdLNHlQAAAAAAAAWdwnjvtjVaRpbnseHHn53z9vZB3SAAAAAAAAAAAAACEgK2/wD/AB/3Ys5o7/PB/dGcAAoAAAAAAtfD74svy/u0FD4dj4sr6Sad19BCQAAAAAAAAAAAAAABz281wyn/AFrJbNjHzx0tnS6AgBQAAAAAkBf+Hzw29clpz3fDhwxl8/V1QAAAAAAAAAAAAAAAAFLf9l5ZSeXnp0XUWAxha2+52S2XWTW6XlZFVQAAAAWNy2VuUvpOf1eNhsLneknnWls8JjJJ5T7+6D0kAAAAAAAAAAAAAAQCRFrxdrjPxTuDoOGW94T8Wvyjllv2Ppjb9gd95vgy+TKd9tvWWUs0klcFAAAAF74deWU95+i4ydhtrjrppz6rOO/T1x7VBdFeb5h1s+ce8dvhfxQHUeZlOsv1egAAAAAAAAAU983jTwzz9b09gdNvvWOPL+a9J6fNT2m9Z3109pycQC29UaJFAAAAAAAAAAAAB0w2+c8sr8rzjmAvbHfZeWU0955f4W5WMsbrvHDdL/Lfsg0hCQAAAAc9vtOHG325fNk2+t875rvxDK+HGS9byU+G9L2BAnhvS9jhvS9lECeG9L2OG9L2BAnhvS9jhvS9gQJ4b0vY4b0vYECeG9L2OG9L2BAnhvS9jhvS9gQJ4b0vY4b0vYECeG9L2OG9L2BAnhvS9jhvS9gQJ4b0vY4b0vYF/cNrrjcb54/otMzdLcc5yvPleXVpoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP/9k=';
 
-                // set user entity data
-                $user->setUsername($username); // set user username
-                $user->setPassword($hashed_password); // set hashed password
-                $user->setRole("owner"); // set default role
-                $user->setIpAddress($ip_address); // set user ip
-                $user->setToken($token); // set token (for auth validation)
-                $user->setRegistedTime($date); // set register time
-                $user->setLastLoginTime('not logged'); // set login date
-                $user->setProfilePic($image_base64); // set profile pic (base64)
-                $user->setVisitorId($visitor_id); // set visitor id
+                    // set user entity data
+                    $user->setUsername($username); // set user username
+                    $user->setPassword($hashed_password); // set hashed password
+                    $user->setRole("owner"); // set default role
+                    $user->setIpAddress($ip_address); // set user ip
+                    $user->setToken($token); // set token (for auth validation)
+                    $user->setRegistedTime($date); // set register time
+                    $user->setLastLoginTime('not logged'); // set login date
+                    $user->setProfilePic($image_base64); // set profile pic (base64)
+                    $user->setVisitorId($visitor_id); // set visitor id
 
-                // log regstration event
-                $this->logManager->log('authenticator', 'registration new user: '.$username.' registred');
+                    // log regstration event
+                    $this->logManager->log('authenticator', 'registration new user: '.$username.' registred');
 
-                // insert new user
-                try {
-                    $this->entityManager->persist($user);
-                    $this->entityManager->flush();
-                } catch (\Exception $e) {
-                    $this->errorManager->handleError('error to insert new user: '.$e->getMessage(), 400);
+                    // insert new user
+                    try {
+                        $this->entityManager->persist($user);
+                        $this->entityManager->flush();
+                    } catch (\Exception $e) {
+                        $this->errorManager->handleError('error to insert new user: '.$e->getMessage(), 400);
+                    }
+
+                    // set user token (login-token session)
+                    $this->authManager->login($username, $user->getToken(), false);
+
+                    // redirect to homepage
+                    return $this->redirectToRoute('admin_dashboard');
                 }
-
-                // set user token (login-token session)
-                $this->authManager->login($username, $user->getToken(), false);
-
-                // redirect to homepage
-                return $this->redirectToRoute('admin_dashboard');
             }
 
             // render default register view
