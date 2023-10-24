0ecc9ef80aa5ef6ee0ea216b6ddfff87b4d0fbf7
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-24 10:04:40 +0200

    set maker bundle for all env

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
