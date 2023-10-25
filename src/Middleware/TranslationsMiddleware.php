9eab4c11ae97c68fc8698d6b8451a4b4ae5dab15
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-10-25 16:47:32 +0200

    [+] testing code

--- src/Middleware/TranslationsMiddleware.php
+++ src/Middleware/TranslationsMiddleware.php
@@ -27,10 +27,6 @@ class TranslationsMiddleware
         // get visitor language
         $language = $this->visitorInfoUtil->getVisitorLanguage();
 
-        $locales = $this->translator->getLocales();
-
-        dd($locales);
-
         if ($language == null or $language == 'host' or $language == 'unknown') {
             $this->translator->setLocale('en');
         } else {
