e450f45d789a2abcd1c9a7f5e801ea3bfa827695
Lukáš Bečvář <becvarlukas99@gmail.com>
2023-11-20 18:03:08 +0100

    [+] installed monolog

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
