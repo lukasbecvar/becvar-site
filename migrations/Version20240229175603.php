b32a98cc1eae9eee45daf6adc0cffcecd907a9a1
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-02-29 18:58:42 +0100

    [R] reworked docker env

--- migrations/Version20240229175603.php
+++ migrations/Version20240229175603.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240229100322 extends AbstractMigration
+final class Version20240229175603 extends AbstractMigration
 {
     public function getDescription(): string
     {
