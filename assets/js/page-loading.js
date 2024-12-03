d0d3f65a6e00ed72a26f69fa67ce312a1d806db0
Lukáš Bečvář <lordbecvold@gmail.com>
2024-12-03 16:01:39 +0100

    fixed load component

--- assets/js/page-loading.js
+++ assets/js/page-loading.js
@@ -4,3 +4,19 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("content").style.display = "block"
     document.getElementById("loader-wrapper").style.display = "none"
 })
+
+/* loading component for click on links */
+document.addEventListener("DOMContentLoaded", function () {
+    const loader = document.getElementById("loader-wrapper")
+    loader.style.display = "none"
+    document.body.addEventListener("click", function (event) {
+        const target = event.target.closest("a")
+        if (target && target.href) {
+            event.preventDefault()
+            loader.style.display = "flex"
+            setTimeout(() => {
+                window.location.href = target.href
+            }, 10)
+        }
+    })
+})
